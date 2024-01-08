e88cc18d631d5c69aeb9fba489f8eead54b1f512
bouchandre <ab.alexandreboucher@gmail.com>
2024-01-08 17:08:19 -0500

    Update MotorUtility.cs

--- Runtime/MotorUtility.cs
+++ Runtime/MotorUtility.cs
@@ -25,9 +25,9 @@ namespace KinematicMovementSystem.Utility
 
 		public static void CalculateSlopeValues(ref this GroundReport report, Vector3 characterUp, Vector3 characterRight, Quaternion rotation)
 		{
-			report.SlopeAngleGlobal = Vector3.Angle(report.Normal, characterUp);
-			report.SlopeAngleForward = Vector3.Angle(-characterUp, Vector3.Cross(characterRight, report.Normal)) - 90;
-			report.SlopeDirectionGlobal = Mathf.Atan2(-report.Normal.x, -report.Normal.z) * Mathf.Rad2Deg;
+			report.SlopeAngleGlobal = Vector3.Angle(report.CapsuleNormal, characterUp);
+			report.SlopeAngleForward = Vector3.Angle(-characterUp, Vector3.Cross(characterRight, report.CapsuleNormal)) - 90;
+			report.SlopeDirectionGlobal = Mathf.Atan2(-report.CapsuleNormal.x, -report.CapsuleNormal.z) * Mathf.Rad2Deg;
 			report.SlopeDirectionLocal = Mathf.DeltaAngle(rotation.eulerAngles.y, report.SlopeDirectionGlobal);
 
 
